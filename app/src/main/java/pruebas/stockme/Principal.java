063a5a9898cef25b6515e63ad3b62ec196bda6de
Pablo Tobes <pablotobes@hotmail.com>
2016-02-25 17:30:22 +0100

    Comentario

--- app/src/main/java/pruebas/stockme/Principal.java
+++ app/src/main/java/pruebas/stockme/Principal.java
@@ -17,7 +17,7 @@ public class Principal extends AppCompatActivity {
     private Button btnLateral;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//Comentario
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
