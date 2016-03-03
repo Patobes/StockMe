b6762d45d40e47f9aeb0166cda5229f59580efb6
Pablo Tobes <pablotobes@hotmail.com>
2016-03-03 16:42:59 +0100

    Activity listas

--- app/src/main/java/stockme/PrincipalConMenuLateral.java
+++ app/src/main/java/stockme/PrincipalConMenuLateral.java
@@ -25,14 +25,14 @@ public class PrincipalConMenuLateral extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.listacompra_add);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.listacompra_add);
+        /*fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
