4eb7ae30d6d06f10e9ae001c9603d4797ce7fc81
Pablo Tobes <pablotobes@hotmail.com>
2016-03-16 12:43:46 +0100

    Se añaden listas

--- app/src/main/java/stockme/stockme/ListaAdd.java
+++ app/src/main/java/stockme/stockme/ListaAdd.java
@@ -1,13 +1,23 @@
 package stockme.stockme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Date;
+
+import stockme.stockme.logica.Lista;
+import stockme.stockme.persistencia.BDHandler;
 
 public class ListaAdd extends AppCompatActivity {
+    private Button btn_aceptar;
+    private Button btn_cancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,37 @@ public class ListaAdd extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         this.setTitle("Crear lista");
+
+        btn_aceptar = (Button)findViewById(R.id.lista_add_btn_aceptar);
+        btn_aceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nombre = ((EditText)findViewById(R.id.lista_add_et_nombre)).getText().toString();
+
+                if (!nombre.matches("")) {
+                    String fecha = new Date().toString();
+
+                    Lista nueva = new Lista(nombre, fecha, fecha);
+                    BDHandler manejador = new BDHandler(v.getContext());
+
+                    manejador.insertarLista(nueva);
+
+                    finish();
+                }
+
+            }
+        });
+
+        btn_cancelar = (Button)findViewById(R.id.lista_add_btn_cancelar);
+        btn_cancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
     }
 
 }
--- app/src/main/java/stockme/stockme/Principal.java
+++ app/src/main/java/stockme/stockme/Principal.java
@@ -1,5 +1,6 @@
 package stockme.stockme;
 
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
