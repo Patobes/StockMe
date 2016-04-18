e4c2584568d3a3552e902d0de4b89e1beb1ff4b0
Tobes <pablotobes@hotmail.com>
2016-04-18 23:18:05 +0200

    Draven iconos, botones en articulo listas y copiada funcionalidad

--- app/src/main/java/stockme/stockme/ArticulosAdd.java
+++ app/src/main/java/stockme/stockme/ArticulosAdd.java
@@ -1,25 +1,38 @@
 package stockme.stockme;
 
+import android.app.Activity;
+import android.content.Context;
 import android.net.Uri;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import java.util.Date;
+
+import stockme.stockme.logica.Lista;
+import stockme.stockme.persistencia.BDHandler;
 import stockme.stockme.util.OpcionesMenus;
+import stockme.stockme.util.Util;
 
 public class ArticulosAdd extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
         Fragment_listas.OnFragmentInteractionListener {
     private EditText etNombre;
     private EditText etPrecio;
     private Spinner spTipos;
+    private Button btn_aceptar;
+    private Button btn_cancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +54,49 @@ public class ArticulosAdd extends AppCompatActivity implements NavigationView.On
         etNombre = (EditText)findViewById(R.id.articulos_add_et_nombre);
         etPrecio = (EditText)findViewById(R.id.articulos_add_et_precio);
         spTipos = (Spinner)findViewById(R.id.articulos_add_sp_tipos);
+
+        btn_aceptar = (Button)findViewById(R.id.lista_add_btn_aceptar);
+        btn_aceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* TODO Cambiar esto para que añada a la lista correctamente
+                Context context = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
+
+                String nombre = ((EditText) findViewById(R.id.lista_add_et_nombre)).getText().toString();
+
+                if (!nombre.matches("")) {
+                    String fecha = Util.diaMesAnyo.format(new Date());
+
+                    Spinner spinner = (Spinner) findViewById(R.id.lista_add_spinner_supermercados);
+                    String supermercado = spinner.getSelectedItem().toString();
+
+                    Lista nueva = new Lista(nombre, fecha, fecha, supermercado);
+                    BDHandler manejador = new BDHandler(v.getContext());
+
+                    if (!manejador.insertarLista(nueva))
+                        Toast.makeText(v.getContext(), "Ya existe la lista '" + nueva.getNombre() + "'", Toast.LENGTH_SHORT).show();
+                    else {
+                        setResult(Activity.RESULT_OK);
+                        finish();
+                    }
+                } else {
+                    CharSequence text = "¡Falta un nombre!";
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }*/
+
+            }
+        });
+
+        btn_cancelar = (Button)findViewById(R.id.lista_add_btn_cancelar);
+        btn_cancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.tipos_array,
                         android.R.layout.simple_spinner_item);
